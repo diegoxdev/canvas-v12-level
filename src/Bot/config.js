3a519db65d2725e0dcd1e993be4e28400d3d552f
Alp Can (diego) <84812072+diegoxdev@users.noreply.github.com>
2021-07-03 21:34:06 +0300

    Create config.js

---
+++ src/Bot/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+Bot: {
+  "botOwner": [""], //Bot sahibini yazınız (Array olabilir)
+  "botPrefix": "", //Botunuzun prefixini yazınız.
+  "botToken": "" //Botunuzun tokenini yazınız.
+},
+Client: {
+  "botDurum":"", //Botunuza bir durum yazınız
+  "botStatus": "", //Statuslar ==> idle / online / dnd
+  "botEylem": "" //Eylemler ==> WATCHING / LISTENING / PLAYING
+},
+Altyapı: {
+  "merhaba": "AUJSLWSANHG", //Değiştirdiğiniz taktirde bot giriş yapmayacaktır. Ellemeyin!
+  "embedColor": "#", //Embedlerde kullanılacak renk kodu, değiştirebilirsiniz!
+  "mongoUrl": "", //Mongo connect URLniz
+  "mongoName": "" //Mongo isminiz (İsteğe bağlı)
+},
+
+}
